c4d672e41016a8f83110889ba41727543920da5f
allen-frye <116049759+allen-frye@users.noreply.github.com>
2024-05-09 14:33:26 -0500

    Reset test_main.py

--- test_main.py
+++ test_main.py
@@ -25,7 +25,7 @@ def client():
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json == 'FALSE'
+    assert response.json == 'HEALTHY'
 
 
 def test_auth(client):
